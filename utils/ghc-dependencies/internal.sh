4a2447f7e87fa7ccf41461735d56d363efb738c4
Toma Sasaki <netst915@gmail.com>
2025-07-16 13:01:28 +0900

    update

--- utils/ghc-dependencies/internal.sh
+++ utils/ghc-dependencies/internal.sh
@@ -24,7 +24,23 @@ case "$DISTRO" in
 			fi
 			;;
 		ubuntu)
-			echo "UBUNTU"
+			OS_NAME="Ubuntu"
+            UPDATE_COMMAND="$UBUNTU_UPDATE_COMMAND"
+            INSTALL_COMMAND="$UBUNTU_INSTALL_COMMAND"
+            UBUNTU_VERSION=$(. /etc/os-release && echo "$VERSION_ID")
+            if dpkg --compare-versions "$UBUNTU_VERSION" ge "20.04" && dpkg --compare-versions "$UBUNTU_VERSION" lt "20.10"; then
+                VERSION_FRIENDLY="$UBUNTU_2004_2010_VERSION"
+                DEPENDENCIES="$UBUNTU_2004_2010"
+            elif dpkg --compare-versions "$UBUNTU_VERSION" ge "20.10" && dpkg --compare-versions "$UBUNTU_VERSION" lt "23"; then
+                VERSION_FRIENDLY="$UBUNTU_2010_23_VERSION"
+                DEPENDENCIES="$UBUNTU_2010_23"
+            elif dpkg --compare-versions "$UBUNTU_VERSION" ge "23"; then
+                VERSION_FRIENDLY="$UBUNTU_23_VERSION"
+                DEPENDENCIES="$UBUNTU_23"
+            else
+                VERSION_FRIENDLY="$UBUNTU_GENERIC_VERSION"
+                DEPENDENCIES="$UBUNTU_GENERIC"
+            fi
 			;;
 		fedora)
 			OS_NAME="Fedora"
@@ -34,7 +50,17 @@ case "$DISTRO" in
 			DEPENDENCIES="$FEDORA_GENERIC"
 			;;
 		centos)
-			echo "CENTOS"
+			OS_NAME="CentOS"
+            UPDATE_COMMAND="$CENTOS_UPDATE_COMMAND"
+            INSTALL_COMMAND="$CENTOS_INSTALL_COMMAND"
+            CENTOS_VERSION=$(rpm -q --qf '%{VERSION}\n' centos-release | cut -d'.' -f1)
+            if [ "$CENTOS_VERSION" -ge 7 ] && [ "$CENTOS_VERSION" -lt 8 ]; then
+                VERSION_FRIENDLY="$CENTOS_7_8_VERSION"
+                DEPENDENCIES="$CENTOS_7_8"
+            else
+                VERSION_FRIENDLY="$CENTOS_GENERIC_VERSION"
+                DEPENDENCIES="$CENTOS_GENERIC"
+            fi
 			;;
 		alpine)
 			OS_NAME="Alpine"
